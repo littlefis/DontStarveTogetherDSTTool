8e1d2e0b76bd0ff9320de9b8dc91aae168481564
unknown <3160104936@zju.edu.cn>
2021-07-26 16:52:42 +0800

    mv

---
+++ src/cluster_opt.py
@@ -0,0 +1,44 @@
+import configparser
+
+class Cluster(object):
+    def __init__(self):
+        self.cluster_token = None
+
+        self.cluster_conf = None
+        self.master_conf = None
+        self.caves_conf = None
+
+    def upload_cluster(self):
+        pass
+
+    def download_cluster(self):
+        pass
+
+    def load_server_cluster(self):
+        pass
+
+    def edit_admin_list(self):
+        # https://www.bilibili.com/video/BV1zy4y1g7S4
+        pass
+
+    def edit_block_list(self):
+        # http://www.eeyy.com/raiders/89990.html
+        pass
+
+    def edit_white_list(self):
+        # http://www.eeyy.com/raiders/89991.html
+        pass
+
+    def edit_cluster_conf(self):
+        pass
+
+    def edit_mod_conf(self):
+        # https://forums.kleientertainment.com/forums/topic/63723-guide-how-to-installconfigure-and-update-mods-on-dedicated-server/
+        pass
+
+
+    def start_server(self):
+        pass
+
+    def stop_server(self):
+        pass
